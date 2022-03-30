2388d9c7b15264e2735373c64313180e40286db3
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-30 21:41:16 +0800

    Removed strong parameters for devise_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
-  # To edit strong parameters in devise forms
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
     @users = User.all
@@ -13,10 +11,5 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  # Method used to add permitted attributes for devise
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name mobile_number approved admin])
   end
 end
