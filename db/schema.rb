9d4ec4a4d3d33550c1dc4b7ee0deac72e66dbdbd
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-31 20:36:04 +0800

    Add users and traders to table

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_094653) do
+ActiveRecord::Schema.define(version: 2022_03_30_153249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "traders", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "mobile_number"
     t.boolean "approved", default: false
-    t.boolean "admin", default: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_traders_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -32,8 +36,11 @@ ActiveRecord::Schema.define(version: 2022_03_30_094653) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "traders", "users"
 end
