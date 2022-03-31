fe3fe0c5efd0583b6b4b05b125752e03207e4555
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-03-31 23:03:44 +0800

    Add controller configuration for registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :admins
   resources :traders
-  devise_for :users
 
   root 'home#index'
 end
