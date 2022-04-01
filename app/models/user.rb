a2acc2335e0648421f00618834589f610e9a6358
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-01 22:34:08 +0800

    add enum for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum role: %i[admin trader]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
