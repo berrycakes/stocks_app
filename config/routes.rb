32c5cc6035cb1feb77c0c1f4a81b31fed4ac1855
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-01 15:50:22 +0800

    Created a devise_scope for :user Add get request for creating a new admin Nested devise_for under devise_scope user Added comments/documentations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_scope :user do
+    get '/admins/new_user', to: 'users/registrations#new_admin', as: 'new_user_admin'
+    devise_for :users, controllers: {
+      # Used to modify controller for registrations
+      # e.g. sign up and account update
+      # Create a new view file under views/users folder for the controller you are going to modify
+      registrations: 'users/registrations'
+    }
+  end
   resources :admins
   resources :traders
 
