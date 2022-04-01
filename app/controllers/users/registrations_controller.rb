46d4d408d9c5e847374ef014d6d791307baf3046
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-01 15:55:10 +0800

    Add skip_before_action for require_no_authentication for new and create (Check documentation/comments for details) Add new method new_admin to build admin when creating UserAdmin. Add an alias for Parent method New Disabled automatic sign in upon succesful sign up Added documentation/comments

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,9 +4,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  # Skips the method :require_no_authentication for new and create
+  # Used to allow looged in Users to create another user
+  skip_before_action :require_no_authentication, only: %i[new create]
+
+  # creates an alias 'devise_new' from Parent Class method 'new' so the 'new' method of Parent Class can be called in other methods in the children class
+  alias devise_new new
+
   # GET /resource/sign_up
+  # Used for nesting fields for trader under User form
   def new
     super(&:build_trader)
+  end
+
+  def new_admin
+    devise_new do |user|
+      user.build_admin
+    end
   end
 
   # POST /resource
@@ -49,6 +63,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
+
+  # Overrides sign_up method of RegistrationsController that logs user in after signing up
+  def sign_up(resource_name, resource); end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
