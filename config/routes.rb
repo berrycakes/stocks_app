19ff8cb02447f6f9f572ed1fd603f3abe00dd185
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-01 22:42:29 +0800

    Removed devise_scope and custom path

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  # Devise scope is used to allow creation of customized paths
-  devise_scope :user do
-    get '/admins/new_user', to: 'users/registrations#new_admin', as: 'new_user_admin'
-    devise_for :users, controllers: {
-      # Used to modify controller for registrations
-      # e.g. sign up and account update
-      # Create a new view file under views/users folder for the controller you are going to modify
-      registrations: 'users/registrations'
-    }
-  end
+  devise_for :users, controllers: {
+    # Used to modify controller for registrations
+    # e.g. sign up and account update
+    # Create a new view file under views/users folder for the controller you are going to modify
+    registrations: 'users/registrations'
+  }
+
   resources :admins
   resources :traders
 
