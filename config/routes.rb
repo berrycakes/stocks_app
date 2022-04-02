e6f7df0c91bf6af0b68fae55d0162e9afe28e66c
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-02 12:49:02 +0800

    Grouped resources near each other. Put devise_for on top

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-
-  resources :stocks
-  post 'search', to: 'stocks#search'
-  post 'calculate', to: 'stocks#calculate'
-  
   devise_for :users, controllers: {
     # Used to modify controller for registrations
     # e.g. sign up and account update
     # Create a new view file under views/users folder for the controller you are going to modify
     registrations: 'users/registrations'
   }
+
+  resources :stocks
+  post 'search', to: 'stocks#search'
+  post 'calculate', to: 'stocks#calculate'
 
   resources :admins
   resources :traders
