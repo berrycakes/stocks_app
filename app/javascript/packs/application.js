f36624b27c19219660c4962779e61703d2c3b3b0
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-02 12:43:31 +0800

    Formatted using prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from '@rails/ujs'
-import Turbolinks from 'turbolinks'
-import * as ActiveStorage from '@rails/activestorage'
-import 'channels'
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
 // to make chart, installed gem
 // import 'chartkick/chart.js'
-import ApexCharts from 'apexcharts'
-window.ApexCharts = ApexCharts
+import ApexCharts from 'apexcharts';
+window.ApexCharts = ApexCharts;
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
