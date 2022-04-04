e2720a0f98261f882c1f4fbd39ec1e283e7894fb
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-04 18:06:39 +0800

    Add http method for editing a trader as an admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    # Used to modify controller for registrations
-    # e.g. sign up and account update
-    # Create a new view file under views/users folder for the controller you are going to modify
-    registrations: 'users/registrations'
-  }
+  devise_scope :user do
+    get '/admins/traders/:id/edit', to: 'users/registrations#edit_trader', as: 'admin_edit_trader'
+    patch '/admins/traders/:id', to: 'users/registrations#update_trader', as: 'admin_update_trader'
+
+    devise_for :users, controllers: {
+      # Used to modify controller for registrations
+      # e.g. sign up and account update
+      # Create a new view file under views/users folder for the controller you are going to modify
+      registrations: 'users/registrations'
+    }
+  end
 
   resources :stocks
   post 'search', to: 'stocks#search'
