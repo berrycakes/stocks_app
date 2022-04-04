20c9af46659c3826b199073014113e8b13679b42
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-04 19:01:48 +0800

    Add dependent option in has_one

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :trader
-  has_one :admin
+  has_one :trader, dependent: :destroy
+  has_one :admin, dependent: :destroy
   accepts_nested_attributes_for :trader
   accepts_nested_attributes_for :admin
 end
