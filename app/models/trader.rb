32adb1952c62beb69f9b76ae78e16b7dd0908be3
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-06 19:31:38 +0800

    Add profit_loss method to show realized profit/loss

--- app/models/trader.rb
+++ app/models/trader.rb
@@ -4,4 +4,17 @@ class Trader < ApplicationRecord
   has_many :wallet_transactions, through: :wallet
   has_many :transactions
   has_many :stocks, through: :transactions
+
+  def profit_loss
+    total_balance = wallet.balance
+    transactions.each do |transaction|
+      case transaction.transaction_type
+      when 'buy'
+        total_balance += transaction.current_amount
+      when 'sell'
+        total_balance -= transaction.current_amount
+      end
+    end
+    total_balance
+  end
 end
