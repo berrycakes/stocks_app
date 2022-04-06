f8ff9aae5d72ad2d54b8d0d80ddfa5591fe0c3f4
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-06 18:50:10 +0800

    Add has_many relationship between stocks and traders through transactions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,60 +1,61 @@
 class Stock < ApplicationRecord
-    has_many :transactions
-    accepts_nested_attributes_for :transactions
+  has_many :transactions
+  has_many :traders, through: :transactions
+  accepts_nested_attributes_for :transactions
 
-    @@market_data = []
+  @@market_data = []
 
-    def self.get_market_data(currency)
-        # ids = Stock.select(:slug).map(&:slug).join(",")
-        url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=#{currency.parameterize}&order=market_cap_desc&per_page=250&page=1&sparkline=true"
-        request = HTTParty.get(url)
-        response = JSON.parse(request.body)
-    end
-  
-    def self.set_market_data
-        @@market_data = get_market_data("php")
-    end
-    
-    set_market_data
+  def self.get_market_data(currency)
+    # ids = Stock.select(:slug).map(&:slug).join(",")
+    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=#{currency.parameterize}&order=market_cap_desc&per_page=250&page=1&sparkline=true"
+    request = HTTParty.get(url)
+    response = JSON.parse(request.body)
+  end
 
-    def get_data
-        data =  @@market_data.select {|coin| coin["symbol"] == self.symbol.to_s }
-    end
-    
-    def current_price
-        self.get_data()[0].dig("current_price")
-    end
+  def self.set_market_data
+    @@market_data = get_market_data('php')
+  end
 
-    def current_market_cap
-        self.get_data()[0].dig("market_cap")
-    end
+  set_market_data
 
-    def current_daily_high
-        self.get_data()[0].dig("high_24h")
-    end
+  def get_data
+    data = @@market_data.select { |coin| coin['symbol'] == symbol.to_s }
+  end
 
-    def current_daily_low
-        self.get_data()[0].dig("low_24h")
-    end
+  def current_price
+    get_data[0].dig('current_price')
+  end
 
-    def current_daily_change
-        self.get_data()[0].dig("price_change_percentage_24h")
-    end
+  def current_market_cap
+    get_data[0].dig('market_cap')
+  end
 
-    def current_ath
-        self.get_data()[0].dig("ath")
-    end
+  def current_daily_high
+    get_data[0].dig('high_24h')
+  end
 
-    # used for minicharts
-    def weekly_sparkline
-        self.get_data()[0].dig("sparkline_in_7d", "price")
-    end
+  def current_daily_low
+    get_data[0].dig('low_24h')
+  end
 
-    # used for individual big charts
-    def get_ohlc
-        url = "https://api.coingecko.com/api/v3/coins/#{self.slug}/ohlc?vs_currency=php&days=7"
-        request = HTTParty.get(url)
-        response = JSON.parse(request.body)
-        response.map { |x| [Time.at(x.shift).to_datetime,  x.drop(0)]}
-    end
+  def current_daily_change
+    get_data[0].dig('price_change_percentage_24h')
+  end
+
+  def current_ath
+    get_data[0].dig('ath')
+  end
+
+  # used for minicharts
+  def weekly_sparkline
+    get_data[0].dig('sparkline_in_7d', 'price')
+  end
+
+  # used for individual big charts
+  def get_ohlc
+    url = "https://api.coingecko.com/api/v3/coins/#{slug}/ohlc?vs_currency=php&days=7"
+    request = HTTParty.get(url)
+    response = JSON.parse(request.body)
+    response.map { |x| [Time.at(x.shift).to_datetime, x.drop(0)] }
+  end
 end
--- app/models/trader.rb
+++ app/models/trader.rb
@@ -2,4 +2,6 @@ class Trader < ApplicationRecord
   belongs_to :user
   has_one :wallet
   has_many :wallet_transactions, through: :wallet
+  has_many :transactions
+  has_many :stocks, through: :transactions
 end
