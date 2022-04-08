032b9358e6e57fbd4aa947f8b023926d85f3b851
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-08 20:44:37 +0800

    Edit config for to cater to mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,19 +74,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name: ENV['SENDMAIL_USERNAME'],
-    password: ENV['SENDMAIL_PASSWORD'],
-    domain: 'gmail.com',
-    address: 'smtp.gmail.com',
-    port: 465,
-    authentication: :plain,
-    ssl: true,
-    tsl: true,
-    enable_starttls_auto: true
-  }
+  config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+  config.action_mailer.raise_delivery_errors = false
 end
