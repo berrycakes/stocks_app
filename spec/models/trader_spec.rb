854f8159862d03c93139f95e2461dd39324472b5
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-12 23:49:12 +0800

    Refactored code with help of factory_bot

--- spec/models/trader_spec.rb
+++ spec/models/trader_spec.rb
@@ -1,43 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Trader, type: :model do
-  describe '#profit_loss' do
-    it 'shows profit or loss' do 
-        trader = Trader.new
-        trader.id = 1
-        trader.build_wallet
-        trader.wallet.balance = 200
-        stock = Stock.create(
-            slug: 'bitcoin',
-            symbol: 'btc',
-            name: 'Bitcoin',
-            image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-            max_supply: 21000000
-        )
-        transaction = stock.transactions.build(trader_id: 1, transaction_type: 'buy', stock_share: 1, price: 10)
+  # wallet_transaction associated with trader through factory
+  # Wallet transaction deposits 1000
+  let!(:wallet_transaction) { create(:wallet_transaction) }
+  let!(:transaction) { build(:transaction) }
 
-        expect(trader.profit_loss).to eql(200)
+  describe '#profit_loss' do
+    it 'shows profit or loss' do
+      expect(transaction.trader.profit_loss).to eql(1000)
     end
   end
 
   describe '#percent_change' do
-    it 'shows change in percentage' do 
-        wallet = Wallet.create(balance: 200)
-        trader = wallet.create_trader
-        wallet_transaction = WalletTransaction.create(transaction_type: 'Deposit', amount: 200)
-        wallet.wallet_transactions << wallet_transaction
-        # stock = Stock.create(
-        #     slug: 'bitcoin',
-        #     symbol: 'btc',
-        #     name: 'Bitcoin',
-        #     image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
-        #     max_supply: 21000000
-        # )
-        # transaction = stock.transactions.build(trader_id: 1, transaction_type: 'buy', stock_share: 1, price: 10)
-        # p wallet.wallet_transactions
-        expect(WalletTransaction.total_deposit).to include(wallet_transaction)
-        # expect(trader.percent_change).to be('0.0%')
+    it 'shows change in percentage' do
+      expect(transaction.trader.percent_change).to eql('0.0%')
     end
   end
-
 end
