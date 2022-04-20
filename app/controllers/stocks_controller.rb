84d4c32a69aaa1c1b43713eb0733b8f7608561b6
Jianne Gabriel <jiannegabriel@gmail.com>
2022-04-20 17:51:45 +0800

    Add ransack

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,15 +2,17 @@ class StocksController < ApplicationController
   before_action :set_stock, only: %i[ show edit update destroy get_market_data watchlist]
 
   def index
-    # @stocks = Stock.page(params[:page])
-    @stocks = Stock.search(params[:search])
+    # @stocks = Stock.search(params[:search])
+    # if @stocks.class == Array
+    #   @stocks = Kaminari.paginate_array(@stocks).page(params[:page])
+    # else
+    #   @stocks = @stocks.page(params[:page]) 
+    # end
 
+    @q = Stock.ransack(params[:q])
+    @stocks = @q.result().search(params[:search]).page(params[:page]) 
 
-    if @stocks.class == Array
-      @stocks = Kaminari.paginate_array(@stocks).page(params[:page])
-    else
-      @stocks = @stocks.page(params[:page]) 
-    end
+    # @search = Stock.search(params[:search])
 
   end
 
