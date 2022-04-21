d7a9e26fa7ddc68e6a400f6b02aa5b15b156a1ac
Godfrey Peralta <godfreydjperalta@gmail.com>
2022-04-21 10:54:11 +0800

    Add post request of transaction

--- spec/requests/transaction_spec.rb
+++ spec/requests/transaction_spec.rb
@@ -6,14 +6,16 @@ RSpec.describe '/traders', type: :request do
   let(:trader) { transaction.trader }
 
   context 'admin' do
-    it 'shows all the transactions' do
-      sign_in admin
-      get transactions_path
+    describe 'GET /index' do
+      it 'shows all the transactions' do
+        sign_in admin
+        get transactions_path
 
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include('List of Transactions')
-      expect(response.body).to include('Solana')
-      expect(response.body).to include('BUY')
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include('List of Transactions')
+        expect(response.body).to include('Solana')
+        expect(response.body).to include('BUY')
+      end
     end
   end
 
@@ -51,6 +53,22 @@ RSpec.describe '/traders', type: :request do
         expect(response.body).to include('Portfolio')
         expect(response.body).to include('Profit & Loss')
         expect(response.body).to include('Solana')
+      end
+    end
+
+    describe 'POST /transactions' do
+      it 'creates a transaction to buy a stock' do
+        post '/transactions',
+             params: { transaction: { stock_id: 1, trader_id: 1, transaction_type: 'buy', stock_share: 5, date: Time.now,
+                                      price: 100 } }
+
+        expect(response).to redirect_to(/transactions/)
+        follow_redirect!
+
+        expect(response.body).to include('Transaction was successfully created')
+        expect(response.body).to include('BUY ORDER FULFILLED')
+        expect(response.body).to include('Solana')
+        expect(response.body).to include('5')
       end
     end
   end
